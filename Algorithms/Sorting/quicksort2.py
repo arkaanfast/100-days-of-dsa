86dc2ee3298ad645afe6c20f7f1b9281ddcb2f34
arkaanfast <4pa17cs011@pace.edu.in>
2020-03-23 23:50:20 +0530

    added algo

---
+++ Algorithms/Sorting/quicksort2.py
@@ -0,0 +1,33 @@
+# Here we will quick sort but we will keep pivot as the first element
+
+print("Quick sort making first element as pivot")
+
+
+def partition(arr, low, high):
+
+    pivot = arr[low]
+    i = low + 1
+    for j in range(low + 1, high + 1):
+
+        if arr[j] < pivot:
+
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+
+    arr[i - 1], arr[low] = arr[low], arr[i - 1]
+    return (i - 1)
+
+
+def quicksort2(arr, low, high):
+
+    if low < high:
+
+        pivot_position = partition(arr, low, high)
+
+        quicksort2(arr, low, pivot_position - 1)
+        quicksort2(arr, pivot_position + 1, high)
+
+
+arr = [1, 45, 98, 23, 45]
+quicksort2(arr, 0, len(arr) - 1)
+print(arr)
